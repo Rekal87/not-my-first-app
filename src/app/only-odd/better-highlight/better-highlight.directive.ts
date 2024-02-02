bc7c08d119830f46dbf65fd42cc9a6da62227119
Nnamdi <nnamdi.b@gmail.com>
2024-02-02 16:59:01 +0100

    Add color styling to elements in basic and better highlight directives

--- src/app/only-odd/better-highlight/better-highlight.directive.ts
+++ src/app/only-odd/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,10 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
@@ -7,10 +13,29 @@ export class BetterHighlightDirective implements OnInit {
   constructor(private renderer: Renderer2, private elRef: ElementRef) {}
 
   ngOnInit(): void {
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'blue'
+    // );
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
     this.renderer.setStyle(
       this.elRef.nativeElement,
       'background-color',
       'blue'
     );
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'background-color',
+      'transparent'
+    );
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
   }
 }
